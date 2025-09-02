f3ba871b6d64f9065056210748dde7905d7b3a4e
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-09-02 12:23:23 +0700

    'entahlah'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
